0dbe2395e9602263d7e402e894dcf034014af02d
suklaasukkulayt <suklaasukkulayt@gmail.com>
2026-08-30 10:19:01 +0300

    add video support hopefully

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
 document.querySelector("#app").innerHTML = "<p>KuuppaTab intializing...</p>";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+.then(response => response.json())
+.then(data => {
+    
+    let media;
+
+    if (data.media_type === "image") {
+        media = `<img src="${data.url}" style="width: 300px; height: 300px;"/>`;
+    } else if (data.media_type === "video") {
+        media = `<video src="${data.url}" controls style="width: 300px; height: 300px;"></video>`;
+    } else if (data.url.includes("youtube")){
+        media = `<iframe width="300" height="300" src="${data.url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></video>`;
+    }
+    document.querySelector("#app").innerHTML = `
+    <h1>${data.title}</h1>
+    ${media}
+    <p>${data.explanation}</p>
+`;
+})
+.catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+});
